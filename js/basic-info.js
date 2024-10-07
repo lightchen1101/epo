153cc651a239a470731eca1d117b59a35ca72a7a
Pei-Hsuan Ho <93465929+Sherrypei@users.noreply.github.com>
2024-10-07 11:25:18 +0800

    feat : finish destop view & phone view processing on learning-footprint

--- js/basic-info.js
+++ js/basic-info.js
@@ -2,11 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const currentPath = window.location.pathname.split('/').pop();
   const links = document.querySelectorAll('a');
 
-  console.log(currentPath); // 输出当前路径的最后一部分
-
   links.forEach(link => {
     if (link.getAttribute('href') === currentPath) {
       link.classList.add('active');
+      const img = link.querySelector('img');
+      if (img) {
+        const srcParts = img.src.split('/');
+        const filename = srcParts.pop();
+        const newFilename = 'blue-' + filename;
+        srcParts.push(newFilename);
+        img.src = srcParts.join('/');
+      }
     }
   });
+  const updateDisplay = () => {
+    if (window.matchMedia('(max-width: 600px)').matches) {
+      document.querySelectorAll('.phone').forEach(el => el.style.display = 'block');
+      document.querySelectorAll('.desktop').forEach(el => el.style.display = 'none');
+    } else {
+      document.querySelectorAll('.phone').forEach(el => el.style.display = 'none');
+      document.querySelectorAll('.desktop').forEach(el => el.style.display = 'block');
+    }
+  };
+
+  updateDisplay();
+  window.addEventListener('resize', updateDisplay);
+
 });
--- js/course-map.js
+++ js/course-map.js
@@ -1,12 +1,13 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.expand-title img').forEach(img => {
-      img.addEventListener('click', () => {
-        const subContainer = img.closest('.expand-container').querySelector('.expand-sub-container');
-        if (subContainer) {
-          const isExpanded = subContainer.style.display === 'block';
-          subContainer.style.display = isExpanded ? 'none' : 'block';
-          img.src = isExpanded ? 'images/plus.svg' : 'images/plus-1.svg';
-        }
-      });
+document.addEventListener('DOMContentLoaded', () => {
+  
+  document.querySelectorAll('.expand-title img').forEach(img => {
+    img.addEventListener('click', () => {
+      const subContainer = img.closest('.expand-container').querySelector('.expand-sub-container');
+      if (subContainer) {
+        const isExpanded = subContainer.style.display === 'block';
+        subContainer.style.display = isExpanded ? 'none' : 'block';
+        img.src = isExpanded ? 'images/plus.svg' : 'images/plus-1.svg';
+      }
     });
   });
+});
